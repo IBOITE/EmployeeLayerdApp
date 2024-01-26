5734cd4c1db0694bdf231106d475e758f208ca88
iwahbeh.95@hotmail.com <iwahbeh.95@hotmail.com>
2024-01-26 14:37:30 +0300

    add pagination file and its classes for App leb

---
+++ Employee.Application/Pagination/PagingParameters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.Application.Pagination
+{
+    public class PagingParameters
+    {
+        const int maxPageSize = 5;
+        public int PageNum { get; set; } = 1;
+        private int _pageSize = 1;
+        public int PageSize
+        {
+            get
+            {
+                return _pageSize;
+            }
+            set
+            {
+                _pageSize = (value > maxPageSize) ? maxPageSize : value;
+            }
+        }
+    }
+}
